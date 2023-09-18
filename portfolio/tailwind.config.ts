8920a67ad0f4cbbe3a1aabc2748f2240dd96eca1
Arjun <arjun018@e.ntu.edu.sg>
2023-09-18 23:55:50 +0800

    index page

---
+++ portfolio/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms')
+  ],
+}
+export default config
